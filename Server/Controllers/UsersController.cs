c7c33b72b2b4ac0c3581170aedafb163c8a2a551
Umair Hussein <umahus@melinmedical.com>
2020-05-26 22:30:52 +0200

    testing with Db

---
+++ Server/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace AmnasKitchen.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("AmnasKitchenDb"));
+            connection.Open();
+            return connection.QueryFirstOrDefault<string>(@"SELECT top 1 username FROM Tbl_Users");
+        }
+    }
+}
