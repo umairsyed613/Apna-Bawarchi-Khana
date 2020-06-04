098fa7ad47773d91f3c72d1eed3e3dbf6710180f
Umair Hussein <umahus@melinmedical.com>
2020-06-04 18:53:06 +0200

    status db

--- Server/Controllers/StatusController.cs
+++ Server/Controllers/StatusController.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AmnasKitchen.Server.Services;
+using AmnasKitchen.Shared;
+
+using Dapper;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
 namespace AmnasKitchen.Server.Controllers
@@ -13,14 +18,34 @@ namespace AmnasKitchen.Server.Controllers
     [ApiController]
     public class StatusController : ControllerBase
     {
-        public StatusController()
+        private readonly DatabaseConnectionHandler _databaseConnectionHandler;
+
+        public StatusController(DatabaseConnectionHandler connectionHandler)
         {
+            _databaseConnectionHandler = connectionHandler ?? throw new ArgumentNullException(nameof(connectionHandler));
         }
 
         [HttpGet("[action]")]
         public string PingApi()
         {
             return "Pong";
+        }
+
+        [HttpGet("[action]")]
+        public async Task<string> PingDb()
+        {
+            try
+            {
+                using var connection = new SqlConnection(_databaseConnectionHandler.GetDbConnectionString());
+                connection.Open();
+                var data = await connection.QueryAsync<User>(@"SELECT * FROM sa_amna.Users");
+
+                return data?.Count().ToString() ?? "No Data but have connection to Db";
+            }
+            catch
+            {
+                return "No Connection to Database";
+            }
         }
     }
 }
